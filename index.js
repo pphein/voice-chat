3fd13d292b745d4eee582b9ab18d61d897fe1006
pphein <pph.hein87@gmail.com>
2023-06-14 00:07:01 +0630

    Changed socket

--- index.js
+++ index.js
@@ -1,64 +1,48 @@
-const express  = require("express");
-const app = express();
-const handlebars  = require("express-handlebars");
-const http  = require("http").Server(app);
-const io  = require("socket.io")(http);
+var express = require('express');
+var socket = require('socket.io');
 
-// To holding users information
-const socketsStatus = {}
-
-// config and set handlebars to express
-const customHandlebars = handlebars.create({ layoutsDir: "./views"});
-
-app.engine("handlerbars", customHandlebars.engine);
-app.set("view engine", "handlebars");
-
-// enable user access to public folder
-app.use("/files", express.static("public"));
-
-app.get("/home", (req, res) => {
-    res.render("index");
+// App setup
+var app = express();
+var server = app.listen(80, function () {
+  console.log('listening for requests on port 80, https://voice-chat-k7qb.onrender.com');
 });
 
+// Static files
+app.use(express.static('public'));
 
-// Socket 
+//To holding users information 
+const socketsStatus = {};
 
-io.on("connection", function (socket) {
-    const socketId = socket.id;
-    socketsStatus[socket.id] = {};
-  
-  
-    console.log("connect");
-  
-    socket.on("voice", function (data) {
-  
-      var newData = data.split(";");
-      newData[0] = "data:audio/ogg;";
-      newData = newData[0] + newData[1];
-  
-      for (const id in socketsStatus) {
-  
-        if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
-          socket.broadcast.to(id).emit("send", newData);
-      }
-  
-    });
-  
-    socket.on("userInformation", function (data) {
-      socketsStatus[socketId] = data;
-  
-      io.sockets.emit("usersUpdate",socketsStatus);
-    });
-  
-  
-    socket.on("disconnect", function () {
-      delete socketsStatus[socketId];
-    });
-  
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+  console.log('made socket connection', socket.id);
+
+  const socketId = socket.id;
+  socketsStatus[socket.id] = {};
+
+  socket.on("voice", function (data) {
+
+    var newData = data.split(";");
+    newData[0] = "data:audio/ogg;";
+    newData = newData[0] + newData[1];
+
+    for (const id in socketsStatus) {
+
+      if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
+        socket.broadcast.to(id).emit("send", newData);
+    }
+
   });
-  
+
+  socket.on("userInformation", function (data) {
+    socketsStatus[socketId] = data;
+
+    io.sockets.emit("usersUpdate", socketsStatus);
+  });
 
 
-http.listen(6000, () => {
-    console.log("the app is running in port 6000!");
-})
+  socket.on("disconnect", function () {
+    delete socketsStatus[socketId];
+  });
+});
